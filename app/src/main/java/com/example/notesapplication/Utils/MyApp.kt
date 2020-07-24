2abb40ad618f7ff243ca1c6c2de56bf7160468ee
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-07-24 18:25:10 +0530

    Add context appliaction Util

--- app/src/main/java/com/example/notesapplication/Utils/MyApp.kt
+++ app/src/main/java/com/example/notesapplication/Utils/MyApp.kt
@@ -1,10 +1,27 @@
 package com.example.notesapplication.Utils
 
 import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
 class MyApp : Application() {
+
+
+    companion object {
+
+        lateinit var instance: MyApp
+
+
+        fun getApplication(): MyApp {
+            return instance
+        }
+
+        fun getContext(): Context {
+            return instance.applicationContext
+        }
+    }
 
 
     override fun onCreate() {
@@ -23,8 +40,12 @@ class MyApp : Application() {
 
         Realm.setDefaultConfiguration(configurationNotes)
 
-
+        //For getting the application context
+        instance = this
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
     }
 
-
 }
+
+
+
